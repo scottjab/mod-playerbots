220f2016f239983c9fe42c6bb72b6c93ac480f9b
Crow <pengchengw@me.com>
2025-11-28 12:00:12 -0600

    Add missing break in ApplyInstanceStrategies (#1887)
    Well, I hope nobody has tried Magtheridon lately. It looks like this got
    inadvertently deleted during the merge.

--- src/PlayerbotAI.cpp
+++ src/PlayerbotAI.cpp
@@ -1483,18 +1483,6 @@ void PlayerbotAI::ApplyInstanceStrategies(uint32 mapId, bool tellMaster)
         case 544:
             strategyName = "magtheridon";  // Magtheridon's Lair
             break;
-        case 548:
-            strategyName = "ssc";  // Serpentshrine Cavern
-            break;
-        case 550:
-            strategyName = "tk";  // Tempest Keep: The Eye
-            break;
-        case 534:
-            strategyName = "hyjal";  // Hyjal Summit
-            break;
-        case 564:
-            strategyName = "bt";  // Black Temple
-            break;
         case 565:
             strategyName = "gruulslair";  // Gruul's Lair
             break;
